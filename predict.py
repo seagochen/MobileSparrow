249992c9648de75ab161ff260a692e2005e85ce4
Orlando Chen <seagochen@gmail.com>
2025-09-09 10:07:15 +0900

    Deleted obsoleted files, and rewrited the project.

--- predict.py
+++ predict.py
@@ -2,35 +2,29 @@
 @Fire
 https://github.com/fire717
 """
-import os
-import random
-import pandas as pd   
-
-from lib import init, Data, MoveNet, Task
 
 from config import cfg
-
-
-
+from lib import init, Data, MoveNet, Task
 
 
 def main(cfg):
 
     init(cfg)
 
+    model = MoveNet(
+        num_joints=cfg["num_classes"],
+        width_mult=cfg["width_mult"]
+    )
 
-    model = MoveNet(num_classes=cfg["num_classes"],
-                    width_mult=cfg["width_mult"],
-                    mode='train')
-    
+    # model = MoveNet(num_classes=cfg["num_classes"],
+    #                 width_mult=cfg["width_mult"],
+    #                 mode='train')
     
     data = Data(cfg)
     test_loader = data.getTestDataloader()
 
-
     run_task = Task(cfg, model)
-    run_task.modelLoad("output/e91_valacc0.79763.pth")
-
+    run_task.modelLoad("output/best.pt")
 
     run_task.predict(test_loader, "output/predict")
 
