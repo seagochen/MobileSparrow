e959bea253ed2a7af94c7b9c59fe22cffa241422
Orlando Chen <seagochen@hotmail.com>
2025-09-12 17:18:29 +0900

    feat: Modifying SSDLite

--- sparrow/models/heads/movenet_head.py
+++ sparrow/models/heads/movenet_head.py
@@ -5,13 +5,13 @@ class MoveNetHead(nn.Module):
     """
     四头输出：heatmaps / centers / regs / offsets
     """
-    def __init__(self, in_ch, num_joints=17, midc=32, act="sigmoid"):
+    def __init__(self, in_ch, num_joints=17, midc=32):
         super().__init__()
-        Act = nn.Sigmoid if act == "sigmoid" else nn.Sigmoid  # 预留
+
         self.hm = nn.Sequential(nn.Conv2d(in_ch, midc, 3, 1, 1), nn.ReLU(inplace=True),
-                                nn.Conv2d(midc, num_joints, 1), Act())
+                                nn.Conv2d(midc, num_joints, 1), nn.Sigmoid())
         self.ct = nn.Sequential(nn.Conv2d(in_ch, midc, 3, 1, 1), nn.ReLU(inplace=True),
-                                nn.Conv2d(midc, 1, 1), Act())
+                                nn.Conv2d(midc, 1, 1), nn.Sigmoid())
         self.reg = nn.Sequential(nn.Conv2d(in_ch, midc, 3, 1, 1), nn.ReLU(inplace=True),
                                  nn.Conv2d(midc, num_joints*2, 1))     # (dx, dy)
         self.off = nn.Sequential(nn.Conv2d(in_ch, midc, 3, 1, 1), nn.ReLU(inplace=True),
