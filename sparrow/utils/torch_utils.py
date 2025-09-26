390eac39f64f8e67c4a49b6059d5cb5e85f69ef2
Orlando Chen <seagochen@gmail.com>
2025-09-26 11:36:56 +0900

    feat: Updated the training report information

--- sparrow/utils/torch_utils.py
+++ sparrow/utils/torch_utils.py
@@ -125,7 +125,7 @@ def evaluate(model, dataloader, criterion, anchor_generator, precomputed_anchors
         total_loss_cls += loss_cls.item()
         total_loss_reg += loss_reg.item()
 
-        pbar.set_postfix(cls=f"{loss_cls.item():.6f}", reg=f"{loss_reg.item():.6f}")
+        pbar.set_postfix(cls=f"{total_loss_cls:.6f}", reg=f"{total_loss_reg:.6f}")
 
     avg_cls_loss = total_loss_cls / len(dataloader)
     avg_reg_loss = total_loss_reg / len(dataloader)
